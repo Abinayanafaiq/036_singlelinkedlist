bea01041ddfd1b5a1b4a77a6825d6ff653e43ca6
Abinayanafaiq <abinayanafaiq550@gmail.com>
2026-04-23 15:27:52 +0700

    036_singlelinkedlist

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    
+}
